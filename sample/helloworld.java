6657bd7df826ff1f7c95cd90d40f824b376dd60e
karthickcar <36662703+karthickcar@users.noreply.github.com>
2018-02-20 13:38:59 +0000

    Update helloworld.java

--- sample/helloworld.java
+++ sample/helloworld.java
@@ -1,7 +1,7 @@
 public class helloworld
 {
   public static void main(String[] args) {
-        System.out.println("Hello World Anbu");
+        System.out.println("Hello World Anbu kkk  ");
  //creating object
   }
 }
